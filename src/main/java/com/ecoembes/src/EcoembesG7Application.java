78b0efa1e0cf23a9f0f14a0622b47b2897b50825
Asier Amigorena <asier.amigorena@gmail.com>
2025-10-30 13:29:25 +0100

    Commit inicial

---
+++ src/main/java/com/ecoembes/src/EcoembesG7Application.java
@@ -0,0 +1,13 @@
+package com.ecoembes.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoembesG7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoembesG7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecoembes/src/EcoembesG7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoembes.src;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoembesG7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
