b52a7ae569228f6324838c280d8a91afa0fb0f05
Asier Amigorena <asier.amigorena@gmail.com>
2025-11-20 18:29:02 +0100

    Añade proyectos ContSocket y PlasSb

---
+++ PlasSB/src/main/java/com/example/plassb/PlasSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.plassb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlasSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlasSbApplication.class, args);
+    }
+
+}
---
+++ PlasSB/src/test/java/com/example/plassb/PlasSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plassb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlasSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
