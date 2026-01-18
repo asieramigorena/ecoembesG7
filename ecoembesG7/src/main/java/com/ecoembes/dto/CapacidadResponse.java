b856c12c257dadf3eff5674491faf8446fa9254b
Jon Fernández <j.fernandezde@opendeusto.es>
2026-01-18 17:36:24 +0100

    Obtener capacidad PlasSB arreglado.
    Queda por revisas ContSockets

--- ecoembesG7/src/main/java/com/ecoembes/dto/CapacidadResponse.java
+++ ecoembesG7/src/main/java/com/ecoembes/dto/CapacidadResponse.java
@@ -1,8 +1,24 @@
 package com.ecoembes.dto;
 
 public class CapacidadResponse {
+
+    private String id;        // o LocalDate
     private double capacidad;
 
-    public double getCapacidad() { return capacidad; }
-    public void setCapacidad(double capacidad) { this.capacidad = capacidad; }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(double capacidad) {
+        this.capacidad = capacidad;
+    }
 }
+
